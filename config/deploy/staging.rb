60c92503c72496fe08439f2d305ee1a09228bbf3
Tom Naessens <Tom.Naessens@UGent.be>
2015-09-08 14:14:24 +0200

    Add capistrano deployment goodies

---
+++ config/deploy/staging.rb
@@ -0,0 +1,9 @@
+server 'king.ugent.be', user: 'tap', roles: %w{web app db}, ssh_options: {
+  forward_agent: true,
+  auth_methods: ['publickey'],
+  port: 2222
+}
+
+set :rails_env, 'production'
+set :rbenv_type, :system
+set :rbenv_ruby, File.read('.ruby-version').strip
