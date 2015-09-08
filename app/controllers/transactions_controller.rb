20bb2804821c000774b974a86e92206288648032
Ilion Beyst <ilion.beyst@gmail.com>
2015-09-08 17:46:07 +0200

    Make transaction datatable actually work

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
 
   def index
+    p params
     respond_to do |format|
       format.html
       format.json { render json: TransactionDatatable.new(view_context) }
--- app/datatables/transaction_datatable.rb
+++ app/datatables/transaction_datatable.rb
@@ -1,12 +1,10 @@
 class TransactionDatatable < AjaxDatatablesRails::Base
 
   def sortable_columns
-    # Declare strings in this format: ModelName.column_name
-    @sortable_columns ||= []
+    @sortable_columns ||= ['Transaction.id']
   end
 
   def searchable_columns
-    # Declare strings in this format: ModelName.column_name
     @searchable_columns ||= []
   end
 
@@ -14,9 +12,7 @@ class TransactionDatatable < AjaxDatatablesRails::Base
 
   def data
     records.map do |record|
-      [
-        record.id, record.debtor.name, record.creditor.name, record.amount
-      ]
+      [ record.id, record.debtor.name, record.creditor.name, record.amount ]
     end
   end
 
