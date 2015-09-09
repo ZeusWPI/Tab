1957e3854af257536cccae3a0b886d18ca60dae6
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-09-09 21:09:07 +0200

    parse ranges and return some data about the filtering

--- app/controllers/concerns/data_table.rb
+++ app/controllers/concerns/data_table.rb
@@ -3,40 +3,52 @@ module DataTable
   extend ActiveSupport::Concern
 
   def apply_filter(user, params)
-    p AjaxRequest.new(params)
-    selection_to_json(user, user.transactions)
+    params = sanitize_params(params)
+    selection_to_json(user, params[:draw], user.transactions)
   end
 
   private
 
-  class AjaxRequest
-
-    def initialize(params)
-      # Parsing according to https://datatables.net/manual/server-side
-      @draw = params.require(:draw).to_i
-      @start = params.require(:start).to_i
-      @length = params.require(:length).to_i
-      @columns = Hash.new
-      params.require(:columns).each do |i, column|
-        @columns[column.require(:data).to_sym] = {
-          name: column[:name],
-          searchable: column[:searchable] == 'true',
-          orderable: column[:orderable] == 'true',
-          search_value: column.require(:search)[:value]
-        }
+  def sanitize_params(params)
+    # Parsing according to https://datatables.net/manual/server-side
+    clean = {
+      draw: params.require(:draw).to_i,
+      start: params.require(:start).to_i,
+      length: params.require(:length).to_i,
+      columns: Hash.new
+    }
+    params.require(:columns).each do |i, column|
+      type, value = column.require(:search)[:value].split(':')
+      h = clean[:columns][column.require(:data).to_sym] = {
+        name: column[:name],
+        searchable: column[:searchable] == 'true',
+        orderable: column[:orderable] == 'true',
+        type: type
+      }
+      if type == 'number-range'
+        h[:lower], h[:upper] = value.split('~').map &:to_i
+      elsif type == 'date-range'
+        h[:lower], h[:upper] = value.split('~').map &:to_datetime
+      else
+        h[:value] = value
       end
     end
-
+    return clean
   end
 
-  def selection_to_json(user, selection)
-    { data: selection.map { |transaction| {
+  def selection_to_json(user, draw, selection)
+    selection = selection.to_a
+    {
+      draw: draw,
+      recordsTotal: user.transactions.count,
+      recordsFiltered: selection.count,
+      data: selection.map { |transaction| {
         time: transaction.created_at,
         amount: transaction.signed_amount_for(user),
         peer: transaction.peer_of(user).try(:name),
         issuer: transaction.issuer.name,
         message: transaction.message,
-      } }
+      }}
     }
   end
 end
