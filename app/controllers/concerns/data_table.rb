747d014b852cb139a70fa8fcb28dc5c365ecf97f
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-09-09 17:14:06 +0200

    write my own filtering form and parse with an object

--- app/controllers/concerns/data_table.rb
+++ app/controllers/concerns/data_table.rb
@@ -3,11 +3,31 @@ module DataTable
   extend ActiveSupport::Concern
 
   def apply_filter(user, params)
-    p selection_to_json(user, user.transactions)
+    p AjaxRequest.new(params)
     selection_to_json(user, user.transactions)
   end
 
   private
+
+  class AjaxRequest
+
+    def initialize(params)
+      # Parsing according to https://datatables.net/manual/server-side
+      @draw = params.require(:draw).to_i
+      @start = params.require(:start).to_i
+      @length = params.require(:length).to_i
+      @columns = Hash.new
+      params.require(:columns).each do |i, column|
+        @columns[column.require(:data).to_sym] = {
+          name: column[:name],
+          searchable: column[:searchable] == 'true',
+          orderable: column[:orderable] == 'true',
+          search_value: column.require(:search)[:value]
+        }
+      end
+    end
+
+  end
 
   def selection_to_json(user, selection)
     { data: selection.map { |transaction| {
