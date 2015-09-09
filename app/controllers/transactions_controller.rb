3d8b1e9c602fa02062307aa0c299d3fc9ffdf6e5
Ilion Beyst <ilion.beyst@gmail.com>
2015-09-09 13:33:55 +0200

    fix transactions cretate

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,27 +8,33 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @transaction = Transaction.new(set_params.merge(origin: I18n.t('origin.created_by_user')))
-
-    if @transaction.save
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.json { head :created }
+    @transaction = Transaction.new(transaction_params)
+    respond_to do |format|
+      format.html do
+        if @transaction.save
+          flash[:success] = "Transaction created"
+          redirect_to new_transaction_path
+        else
+          render 'new'
+        end
       end
-    else
-      render 'new'
+
+      format.json do
+        head(@transaction.save ? :created : :unprocessable_entity)
+      end
     end
   end
 
   private
 
-  def set_params
+  def transaction_params
     t = params.require(:transaction)
           .permit(:debtor, :creditor, :amount, :message)
 
     t.update({
       debtor: User.find_by(name: t[:debtor]) || User.zeus,
-      creditor: User.find_by(name: t[:creditor]) || User.zeus
+      creditor: User.find_by(name: t[:creditor]) || User.zeus,
+      issuer: current_client || current_user
     })
   end
 end
