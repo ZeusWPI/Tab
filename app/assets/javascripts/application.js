4173275e964bcc6ac819ec4b5902ddef6e298114
Ilion Beyst <ilion.beyst@gmail.com>
2015-09-09 19:37:23 +0200

    make datatable responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
+//= require dataTables/jquery.dataTables
 //= require select2
 //= require turbolinks
 //= require_tree .
