87c2994cf033decd05b461e9148ceb942b85dee5
Ilion Beyst <ilion.beyst@gmail.com>
2015-09-09 12:49:24 +0200

    add issuer to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -15,16 +15,13 @@
 class Transaction < ActiveRecord::Base
   belongs_to :debtor, class_name: 'User'
   belongs_to :creditor, class_name: 'User'
+  belongs_to :issuer, polymorphic: true
 
   after_save    :recalculate_balances
   after_destroy :recalculate_balances
 
   validates :amount, numericality: { greater_than: 0 }
   validate :different_debtor_creditor
-
-  def client
-    Client.find_by name: origin
-  end
 
   private
 
@@ -34,6 +31,8 @@ class Transaction < ActiveRecord::Base
   end
 
   def different_debtor_creditor
-    self.errors.add :base, "Can't write money to yourself" if self.debtor == self.creditor
+    if self.debtor == self.creditor
+      self.errors.add :base, "Can't write money to yourself"
+    end
   end
 end
