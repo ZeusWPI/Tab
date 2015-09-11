24f8050ac95db260959014cd52344370d53542dd
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2015-09-11 23:32:38 +0200

    implement simple wall of shame

--- app/controllers/concerns/statistics.rb
+++ app/controllers/concerns/statistics.rb
@@ -3,7 +3,11 @@ class Statistics < Rails::Application
 
   def shameful_users
     User.where('balance > :amount', amount: config.shameful_balance)
-        .order(:balance)
+        .order(balance: :desc)
+  end
+
+  def total_debt
+    User.where.not(id: User.zeus).where('balance > 0').sum(:balance)
   end
 
   private
