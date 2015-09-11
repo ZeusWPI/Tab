274059d10f58b9be9071626d4b633adb9ea71d22
benji <benj_cous@hotmail.com>
2015-09-11 17:08:46 +0200

    btn - Zeus link - deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'king.ugent.be', user: 'tap', roles: %w{web app db}, ssh_options: {
+server 'king.ugent.be', user: 'tab', roles: %w{web app db}, ssh_options: {
   forward_agent: true,
   auth_methods: ['publickey'],
   port: 2222
