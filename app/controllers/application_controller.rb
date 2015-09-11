29b51a63e262ee8ca45afc3de7eee4e5e5814843
benji <benj_cous@hotmail.com>
2015-09-11 22:21:18 +0200

    i guess noctua removed this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_client
-    @current_client ||= Client.find_by key: request.headers["X-API-KEY"]
+    @current_client ||= Client.find_by key: (request.headers["X_API_KEY"] || request.headers["HTTP_X_API_KEY"])
   end
 
   def current_ability
