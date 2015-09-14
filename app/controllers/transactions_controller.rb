6c04a90f680a5499692cfff34146cd178ce26870
benji <benj_cous@hotmail.com>
2015-09-14 10:32:58 +0200

    Return new transaction on create

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,6 +3,7 @@ class TransactionsController < ApplicationController
 
   before_action :authenticate_user!, except: :create
   before_action :authenticate_user_or_client!, only: :create
+
   respond_to :js, only: :create
 
   def create
@@ -11,7 +12,7 @@ class TransactionsController < ApplicationController
     authorize!(:create, @transaction)
 
     if @transaction.save
-      head :created
+      render json: @transaction, status: :created
     else
       render json: @transaction.errors.full_messages,
         status: :unprocessable_entity
@@ -28,7 +29,7 @@ class TransactionsController < ApplicationController
       debtor: t[:debtor] ? User.find_or_create_by(name: t[:debtor]) : User.zeus,
       creditor: t[:creditor] ? User.find_or_create_by(name: t[:creditor]) : User.zeus,
       issuer: current_client || current_user,
-      amount: (t[:euros].to_f*100 + t[:cents].to_f).to_i,
+      amount: (t[:euros].to_f * 100 + t[:cents].to_f).to_i,
       message: t[:message]
     }
   end
