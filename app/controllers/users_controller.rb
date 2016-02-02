aa2b22358af67d98c329b585598bde88650bba5d
benji <benj_cous@hotmail.com>
2016-02-02 15:24:26 +0100

    Return balance for non-existing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: :show
   before_action :authenticate_user_or_client!, only: :show
 
-  load_and_authorize_resource
+  load_and_authorize_resource except: :show
 
   def show
     @user = User.find_by_name(params[:id]) || User.new
