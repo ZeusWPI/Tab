ccc7b987dd086e5946525838aa8ad9d0886f80c4
benji <benj_cous@hotmail.com>
2016-02-04 13:45:12 +0100

    Return new user instead of nil to avoid parse errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource except: :show
 
   def show
-    @user = User.find_by(name: params[:id])
+    @user = User.find_by(name: params[:id]) || User.new
     respond_to do |format|
       format.html { @transaction = Transaction.new }
       format.json { render json: @user }
