c4569551d941d4b132e20dab46cfb9cff21aadb9
benji <benj_cous@hotmail.com>
2017-01-16 23:16:51 +0100

    FFS rails

--- db/migrate/20170109123717_create_requests.rb
+++ db/migrate/20170109123717_create_requests.rb
@@ -1,9 +1,9 @@
 class CreateRequests < ActiveRecord::Migration
   def change
     create_table :requests do |t|
-      t.references :debtor,   index: true,       null: false
-      t.references :creditor, index: true,       null: false
-      t.references :issuer,   polymorphic: true, index: true, null: false
+      t.references :debtor,   null: false
+      t.references :creditor, null: false
+      t.references :issuer,   polymorphic: true, null: false
       t.integer :amount, null: false, default: 0
       t.string :message
 
@@ -11,6 +11,10 @@ class CreateRequests < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    add_index :requests, :debtor_id   unless index_exists?(:requests, :debtor)
+    add_index :requests, :creditor_id unless index_exists?(:requests, :creditor)
+    add_index :requests, [:issuer_type, :issuer_id] unless index_exists?(:requests, :issuer)
 
     add_foreign_key :requests, :users, column: :creditor_id
     add_foreign_key :requests, :users, column: :debtor_id
