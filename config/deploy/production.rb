a2df79d9d1dc9dde6a0779e7633b4d9ad8b4c672
Rien Maertens <maertensrien@gmail.com>
2018-08-06 19:51:30 +0200

    Tab runs on tab.zeus.gent

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,4 +7,3 @@ server 'zeus.ugent.be', user: 'tab', roles: %w{web app db}, ssh_options: {
 set :rails_env, 'production'
 set :rbenv_type, :system
 set :rbenv_ruby, File.read('.ruby-version').strip
-set :default_env, 'RAILS_RELATIVE_URL_ROOT' => '/tab'
