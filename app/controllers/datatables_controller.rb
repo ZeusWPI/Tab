6a9651efbd3af1082a01c5909f76434c4c1948b6
Rien Maertens <maertensrien@gmail.com>
2018-08-07 20:13:23 +0200

    Hoe heeft dit OOIT gewerkt?

--- app/controllers/datatables_controller.rb
+++ app/controllers/datatables_controller.rb
@@ -2,7 +2,7 @@ class DatatablesController < ApplicationController
   respond_to :json
 
   def transactions_for_user
-    user = User.find(params[:id])
+    user = User.find_by(name: params[:id])
     datatable = DataTable.new(user, params)
     render json: datatable.json
   end
