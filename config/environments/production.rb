774efd80f6f5ed6c8c10a871c193dfd40f69f9ec
redfast00 <redfast00@gmail.com>
2019-01-01 13:33:40 +0100

    No double HSTS header

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,9 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # We don't do this, because nginx adds the HSTS header and that conflicts.
+  # Cookies are secure (config/initializers/session_store.rb)
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
