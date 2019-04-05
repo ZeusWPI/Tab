b367d749cad31ccfbcee9d3e8cbbbd000de45e4b
redfast00 <redfast00@gmail.com>
2019-04-05 16:38:42 +0200

    You can reset your own API token

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -7,7 +7,7 @@ class UserAbility
     can :manage, :all if user.penning?
     can :create, Request, creditor_id: user.id
     can [:confirm, :decline], Request, debtor_id: user.id
-    can :read, User, id: user.id
+    can [:read, :reset_key], User, id: user.id
     can :manage, Notification, user_id: user.id
     can :create, Transaction do |t|
       t.debtor == user && t.amount <= Rails.application.config.maximum_amount
