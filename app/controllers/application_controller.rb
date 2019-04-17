4fb4385ba5f3f14c3bde18ccc7c3e262811541eb
Rien Maertens <rien.maertens@posteo.be>
2019-04-18 00:50:41 +0200

    Also skip CSRF check for clients (Tap)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token, if: :api_request?
 
   def api_request?
-    user_token && request.format.json?
+    (user_token.present? || current_client.present?) && request.format.json?
   end
 
   rescue_from CanCan::AccessDenied do |exception|
