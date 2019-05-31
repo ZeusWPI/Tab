f93e588a1f2b9759fbc66f9db11cb8d6b1a44769
Tom Naessens <Tom.Naessens@UGent.be>
2019-05-31 14:50:55 +1200

    Allow the issuer of a request to cancel it

--- app/models/request.rb
+++ app/models/request.rb
@@ -17,7 +17,7 @@
 class Request < ActiveRecord::Base
   include BaseTransaction
 
-  enum status: [:open, :confirmed, :declined]
+  enum status: [:open, :confirmed, :declined, :cancelled]
 
   def confirm!
     return unless open?
@@ -36,6 +36,15 @@ class Request < ActiveRecord::Base
     update_attributes status: :declined
   end
 
+  def cancel!
+    return unless open?
+
+    Notification.create user: creditor, message: cancelled_message unless issuer == creditor
+    Notification.create user: debtor, message: cancelled_message unless issuer == debtor
+
+    update_attributes status: :cancelled
+  end
+
   private
 
   def confirmed_message
@@ -44,5 +53,9 @@ class Request < ActiveRecord::Base
 
   def declined_message
     "#{debtor.name} refuses to pay €#{amount/100.0} for \"#{message}\"."
+  end
+
+  def cancelled_message
+    "#{issuer.name} cancelled the request to pay #{debtor.name} €#{amount/100.0} for \"#{message}\" to #{creditor.name}."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :requests, only: [:index], shallow: true do
       post :confirm
       post :decline
+      post :cancel
     end
     resources :notifications, only: [:index], shallow: true do
       post :read
