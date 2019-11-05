c2a21a00896425dc7e5872b258a6fc9f60ffdc24
redfast00 <redfast00@gmail.com>
2019-11-05 16:08:47 +0100

    Fix getting transactions of other people

--- app/controllers/datatables_controller.rb
+++ app/controllers/datatables_controller.rb
@@ -1,8 +1,13 @@
 class DatatablesController < ApplicationController
+  load_and_authorize_resource :user, find_by: :id
+
+  before_action :load_notification, only: :read
+  authorize_resource :user, only: :read
   respond_to :json
 
   def transactions_for_user
     user = User.find_by(name: params[:id])
+    authorize! :read, user
     datatable = DataTable.new(user, params)
     render json: datatable.json
   end
