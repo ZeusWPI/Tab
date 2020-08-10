1388f922421358413e5b8d5a92b67ee76e0f3842
redfast00 <redfast00@gmail.com>
2020-08-11 01:35:19 +0200

    Begone debts

--- app/models/user_ability.rb
+++ app/models/user_ability.rb
@@ -6,12 +6,14 @@ class UserAbility
 
     can :manage, :all if user.penning?
     can :create, Request, creditor_id: user.id
-    can [:confirm, :decline], Request, debtor_id: user.id
+    can [:confirm, :decline], Request do |r|
+      (debtor_id == user.id) && (user.balance - r.amount >= 0)
+    end
     can :cancel, Request, issuer_id: user.id
     can [:read, :reset_key, :add_registration_token], User, id: user.id
     can :manage, Notification, user_id: user.id
     can :create, Transaction do |t|
-      t.debtor == user && t.amount <= Rails.application.config.maximum_amount
+      t.debtor == user && t.amount <= Rails.application.config.maximum_amount && (user.balance - t.amount >= 0)
     end
   end
 end
