eb50dfa4e47f4399e22a769d8e65aa2bcaabbf12
Tom Naessens <silox@zeus.ugent.be>
2022-05-22 22:16:16 +0200

    Re-add logout button

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,6 +2,7 @@ module FlashHelper
 
   FLASH_TYPES = {
     info: 'Info',
+    notice: 'Notice',
     success: 'Success',
     warning: 'Warning',
     alert: 'Warning',
@@ -15,6 +16,7 @@ module FlashHelper
 
   FLASH_COLOR_CLASSES = {
     info: 'text-blue-700 bg-blue-100',
+    notice: 'text-blue-700 bg-blue-100',
     success: 'text-green-700 bg-green-100',
     warning: 'text-yellow-700 bg-yellow-100',
     alert: 'text-yellow-700 bg-yellow-100',
