761cf073f380eedfc1795e294125fc2e514816e0
Tom Naessens <silox@zeus.ugent.be>
2022-05-22 21:29:42 +0200

    Add flashes back to the application

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 2. Run `yarn`
 3. Run `./bin/dev`
 4. Browse to [http://localhost:3000](http://localhost:3000)
+
+
+## Troubleshooting
+
+### My CSS classes aren't rendered
+
+This probably is a missing entry in Tailwinds configuration. Make sure your file path is included in the `content` array in `tailwind.config.js`.
+
+When Tailwind compiles CSS, it scans all files matching the `content` array to know which CSS classes to include.
---
+++ app/helpers/flash_helper.rb
@@ -0,0 +1,28 @@
+module FlashHelper
+
+  FLASH_TYPES = {
+    info: 'Info',
+    success: 'Success',
+    warning: 'Warning',
+    alert: 'Warning',
+    error: 'Error',
+    dark: 'Dark',
+  }
+
+  def flash_title(key)
+    FLASH_TYPES.fetch(key.to_sym, FLASH_TYPES[:dark])
+  end
+
+  FLASH_COLOR_CLASSES = {
+    info: 'text-blue-700 bg-blue-100',
+    success: 'text-green-700 bg-green-100',
+    warning: 'text-yellow-700 bg-yellow-100',
+    alert: 'text-yellow-700 bg-yellow-100',
+    error: 'text-red-700 bg-red-100',
+    dark: 'text-gray-700 bg-gray-100',
+  }
+
+  def flash_color_classes(key)
+    FLASH_COLOR_CLASSES.fetch(key.to_sym, FLASH_TYPES[:dark])
+  end
+end
