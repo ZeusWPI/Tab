5c1da6b44c7509f694becdbd7060ba76a7c90f33
Tom Naessens <silox@zeus.ugent.be>
2022-05-31 22:58:24 +0200

    Switch back to secrets file instad of credentials file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,8 +273,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :zeuswpi,
-                  Rails.application.credentials.omniauth_client_id,
-                  Rails.application.credentials.omniauth_client_secret,
+                  Rails.application.secrets.omniauth_client_id,
+                  Rails.application.secrets.omniauth_client_secret,
                   token_params: { parse: :json }
 
   # ==> Warden configuration
