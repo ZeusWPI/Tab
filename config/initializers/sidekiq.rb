4c617a4154851660e07582e233620958ba956d31
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-02-24 23:31:49 +0100

    chore(config): load sidekiq testing for test env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,7 @@
-Sidekiq::Cron.configure do |_config|
-  # config.natural_cron_parsing_mode = :single
+if ENV["RAILS_ENV"] != "test"
+  Sidekiq::Cron.configure do |_config|
+    # config.natural_cron_parsing_mode = :single
 
-  Sidekiq::Cron::Job.create(name: "Monthly Reminder", cron: "0 0 1 * *", class: "MonthlyReminder")
+    Sidekiq::Cron::Job.create(name: "Monthly Reminder", cron: "0 0 1 * *", class: "MonthlyReminder")
+  end
 end
