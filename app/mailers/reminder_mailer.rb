290b291d943b7c105665332b739d495d22721060
Tom Naessens <silox@zeus.ugent.be>
2025-09-01 09:59:30 +0200

    Only send reminders for open requests

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -3,7 +3,7 @@
 class ReminderMailer < ApplicationMailer
   def request_reminder
     @user = User.find(params[:user_id])
-    @requests = @user.incoming_requests
+    @requests = @user.incoming_requests.open
     mail(to: "#{@user.name}@zeus.ugent.be", subject: "Tab: Monthly request reminders")
   end
 end
