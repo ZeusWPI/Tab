5ea6fc15188c4cbc83807fe137dd6d2cfa686b17
Tom Naessens <silox@zeus.ugent.be>
2025-09-01 10:01:57 +0200

    Only send mails to people with open requests

--- app/actions/monthly_reminder.rb
+++ app/actions/monthly_reminder.rb
@@ -4,8 +4,8 @@ class MonthlyReminder
   include Sidekiq::Worker
 
   def perform(*_args)
-    people = Request.all.group_by(&:debtor_id)
-    people.each_key do |debtor_id|
+    debtor_ids = Request.open.pluck(:debtor_id).uniq
+    debtor_ids.each do |debtor_id|
       ReminderMailer.with(user_id: debtor_id).request_reminder.deliver_later
     end
   end
