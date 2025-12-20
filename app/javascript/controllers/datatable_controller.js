11f17737a9169093f7a12f5b656dfddb6bd0e962
Jonas Meeuws <jonas.meeuws@gmail.com>
2025-12-20 03:40:03 +0100

    fix(frontend): Mostly fix datatables after upstream changes (filters remain broken).

--- app/javascript/controllers/datatable_controller.js
+++ app/javascript/controllers/datatable_controller.js
@@ -2,33 +2,56 @@ import { Controller } from "@hotwired/stimulus";
 
 import { DateTime } from "luxon";
 
-require('datatables.net')();
+import DataTable from "datatables.net";
 
 export default class extends Controller {
   connect() {
     // Initialize the table
-    const table = $('#transactions').DataTable({
-      processing: true,
+    const table = new DataTable("#transactions", {
+      // Query the url set in data-source that supports pagination
       serverSide: true,
-      searching: true,
+      ajax: $('#transactions').data('source'),
+      processing: true,
+
+      // Unsupported by backend
       lengthChange: false,
       ordering: false,
-      ajax: $('#transactions').data('source'),
-      pagingType: 'full_numbers',
-      autoWidth: false,
+      searching: false,
+
       responsive: true,
+
+      // The control elements
+      layout: {
+        topStart: {
+          info: {}
+        },
+        topEnd: {
+          paging: {}
+        },
+      },
+      language: {
+        paginate: {
+          first: 'First',
+          previous: 'Previous',
+          next: 'Next',
+          last: 'Last',
+        }
+      },
+
+      // The table itself
+      autoWidth: false,
       columns: [
-        { data: 'time',    name: 'Time',    width: "20%", className: 'dataTables_cell date_column'},
-        { data: 'amount',  name: 'Amount',  width: "10%", className: 'dataTables_cell amount_column'},
-        { data: 'peer',    name: 'Peer',    width: "15%", className: 'dataTables_cell peer_column'},
-        { data: 'issuer',  name: 'Issuer',  width: "15%", className: 'dataTables_cell issuer_column'},
-        { data: 'message', name: 'Message', width: "40%", className: 'dataTables_cell message_column'}
+        { data: 'time',    name: 'Time',    width: "20%", className: 'dt-cell dt-cell-date'},
+        { data: 'amount',  name: 'Amount',  width: "10%", className: 'dt-cell dt-cell-amount'},
+        { data: 'peer',    name: 'Peer',    width: "15%", className: 'dt-cell dt-cell-peer'},
+        { data: 'issuer',  name: 'Issuer',  width: "15%", className: 'dt-cell dt-cell-issuer'},
+        { data: 'message', name: 'Message', width: "40%", className: 'dt-cell dt-cell-message'},
       ],
       columnDefs: [
         {
           targets: 0,
           render: function(data, type, full, meta) {
-            return DateTime.fromISO(data.replace(' ', 'T')).toLocaleString(DateTime.DATETIME_MED)
+            return DateTime.fromISO(data.replace(' ', 'T')).toLocaleString(DateTime.DATETIME_MED);
           }
         },
         {
@@ -40,17 +63,14 @@ export default class extends Controller {
         {
           targets: 4,
           render: function(data, type, full, meta) {
-            return new Option(data).innerHTML
+            return new Option(data).innerHTML;
           }
         }
       ],
-      "createdRow": function(row, data, dataIndex) {
-        $(row).addClass( 'dataTables_row');
-      }
+      createdRow: function(row, data, dataIndex) {
+        $(row).addClass('dt-row');
+      },
     });
-
-    // Hide the standard search
-    $('.dataTables_filter').hide();
 
     $('#filters-heading').click(function() {
       document.getElementById('filters').classList.toggle('hidden');
@@ -58,7 +78,7 @@ export default class extends Controller {
       const button = document.getElementById('filters-heading').getElementsByTagName('button')[0];
       button.classList.toggle('rounded-xl');
       button.classList.toggle('rounded-t-xl');
-    })
+    });
 
     // Hook up our input listeners to programmatically search on the table
     $('.input-listen').each(function(index, element) {
