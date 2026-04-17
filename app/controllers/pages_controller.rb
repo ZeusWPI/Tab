3a58dc9d9c1d5140de1e2df505f409a1a48c6a25
Hannes Klinckaert <hannes-dev@mailbox.org>
2026-04-17 21:11:22 +0200

    add totals on incoming and outgoing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,12 @@
-# frozen_string_literal: true
-
+# app/controllers/pages_controller.rb
 class PagesController < ApplicationController
   def landing
     query = TransactionsQuery.new(current_user)
     @transactions = ActiveRecord::Base.connection.exec_query(query.query.order(query.arel_table[:time].desc).take(10).project(Arel.star).to_sql)
     @requests = current_user.incoming_requests.open.includes(:creditor).take(10)
+    @total_incoming_amount = current_user.incoming_requests.open.sum(:amount)
     @outgoing_requests = current_user.outgoing_requests.open.includes(:debtor).take(10)
+    @total_outgoing_amount = current_user.outgoing_requests.open.sum(:amount)
     @notifications = current_user.notifications.unread
   end
 
